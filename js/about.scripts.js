f15d90e55f8de136f7545b93ec7242080faf32d6
Eloi-Perez <eloi104@hotmail.com>
2021-07-15 16:41:46 +0100

    misspell

--- js/about.scripts.js
+++ js/about.scripts.js
@@ -1,5 +1,4 @@
 let hash = window.location.hash;
 if (hash === '#submitted') {
-    console.log(hash);
-    document.querySelector('.submitted').classList.remove = 'hidden';
+    document.querySelector('.submitted').classList.remove('hidden');
 }
