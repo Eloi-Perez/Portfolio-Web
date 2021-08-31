4639b829ca5d19b359a0888b8a8b5b58ad1f7861
Eloi-Perez <eloi104@hotmail.com>
2021-08-31 16:45:14 +0100

    work orded plus logos

--- js/work.scripts.js
+++ js/work.scripts.js
@@ -1,10 +1,10 @@
-const link_to_api = document.querySelector('#link_to_api');
+const link_to_api = document.querySelectorAll('.link_to_api');
 const link_to_client = document.querySelector('#link_to_client');
 const film_api = document.querySelector('#Film_API');
-const film_client = document.querySelector('#Film_Client');
-const toggleHoverClient = () => film_client.classList.toggle('hover_link');
+const film_client = document.querySelectorAll('.Film_Client');
+const toggleHoverClient = () => film_client.forEach(el => el.classList.toggle('hover_link'));
 const toggleHoverAPI = () => film_api.classList.toggle('hover_link');
-link_to_api.addEventListener('mouseover', toggleHoverAPI);
-link_to_api.addEventListener('mouseout', toggleHoverAPI);
+link_to_api.forEach(el => el.addEventListener('mouseover', toggleHoverAPI));
+link_to_api.forEach(el => el.addEventListener('mouseout', toggleHoverAPI));
 link_to_client.addEventListener('mouseover', toggleHoverClient);
 link_to_client.addEventListener('mouseout', toggleHoverClient);
