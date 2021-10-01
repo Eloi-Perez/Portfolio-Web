7c137d8b2288009a5320c146da4aaf1fdcc60b4a
Eloi-Perez <eloi104@hotmail.com>
2021-10-01 01:01:30 +0100

    email modal

--- js/contact.scripts.js
+++ js/contact.scripts.js
@@ -1,4 +1,25 @@
+//Form
 let hash = window.location.hash;
 if (hash === '#submitted') {
     document.querySelector('.submitted').classList.remove('hidden');
+}
+
+//Modal
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        MicroModal.init({
+            awaitCloseAnimation: true,
+            disableFocus: true,
+        });
+    } catch (e) {
+        console.log('micromodal error: ', e);
+    }
+});
+
+function sendEmail() {
+    window.location = 'mailto:eloiperezag@gmail.com';
+}
+
+function toClipboard() {
+    navigator.clipboard.writeText('eloiperezag@gmail.com')
 }
